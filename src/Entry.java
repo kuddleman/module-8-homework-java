840d7176fdcfe1cabc75253d98de7a6352bfca6c
Kuddleman <edl1155@gmail.com>
2021-04-04 20:22:55 -0700

    add isDuplicate method in RandomDrawing class

---
+++ src/Entry.java
@@ -0,0 +1,8 @@
+//public class Entry <T> {
+//    private T entry;
+//
+//    public Entry() {
+//        this.entry = null;
+//    }
+//
+//}
--- src/RandomDrawing.java
+++ src/RandomDrawing.java
@@ -1,11 +1,20 @@
-public class RandomDrawing<T> implements RandomDrawingInterface<T> {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
+public class RandomDrawing<T> implements RandomDrawingInterface<T> {
+    private T entry;
     private boolean allowDuplicateEntries;
+    private int drawingSize = 0;
+    private ArrayList<T> entryList;
 
 
 
     public RandomDrawing(boolean allowDuplicateEntries) {
+        this.entry = null;
         this.allowDuplicateEntries = allowDuplicateEntries;
+        this.entryList = new ArrayList<T>();
 
     }
 
@@ -17,9 +26,38 @@ public class RandomDrawing<T> implements RandomDrawingInterface<T> {
         this.allowDuplicateEntries = allowDuplicateEntries;
     }
 
+    public  <T extends Comparable<T>> boolean isDuplicate(T entry) {
+
+        Set<T> arrayValuesSet = new HashSet<T>();
+
+        for(int i = 0; i < array.length; i++) {
+            T value = array[i];
+            if(arrayValuesSet.add(value)) { //if we can add entry to the Set, then isDuplicate == false
+                return false;
+            } else {
+                return true;
+            }
+        }
+
+    }
+
+
+
     @Override
     public boolean addEntry(T entry) {
+        if(allowDuplicateEntries) {
+            entryList.add(entry);
+            return true;
+        } else {
+            if(!isDuplicate(entry)) {  // if isDuplicate method returns false
+                entryList.add(entry );
+
+                return true;
+            }
+        }
         return false;
+
+
     }
 
     @Override
@@ -29,11 +67,15 @@ public class RandomDrawing<T> implements RandomDrawingInterface<T> {
 
     @Override
     public int size() {
-        return 0;
+        return entryList.size();
     }
 
     @Override
     public void displayEntries() {
+        for(T entry : array) {
+            System.out.println(entry);
+        }
+
 
     }
 }
