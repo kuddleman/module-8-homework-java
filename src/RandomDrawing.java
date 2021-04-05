952fd603af9a42be06b90abe4242b9a6dac1f2ec
Kuddleman <edl1155@gmail.com>
2021-04-04 18:14:48 -0700

    add constructor to RandomDrawing class

--- src/RandomDrawing.java
+++ src/RandomDrawing.java
@@ -1,2 +1,39 @@
-public class RandomDrawing {
+public class RandomDrawing<T> implements RandomDrawingInterface<T> {
+
+    private boolean allowDuplicateEntries;
+
+
+
+    public RandomDrawing(boolean allowDuplicateEntries) {
+        this.allowDuplicateEntries = allowDuplicateEntries;
+
+    }
+
+    public boolean doesAllowDuplicateEntries() {
+        return allowDuplicateEntries;
+    }
+
+    public void setAllowDuplicateEntries(boolean allowDuplicateEntries) {
+        this.allowDuplicateEntries = allowDuplicateEntries;
+    }
+
+    @Override
+    public boolean addEntry(T entry) {
+        return false;
+    }
+
+    @Override
+    public T selectWinner(boolean removeWinner) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void displayEntries() {
+
+    }
 }
